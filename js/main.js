f1caa9dbe69f9d10c98f63c924e59d335f07cacd
root <root@genesis.lan>
2020-08-17 21:52:35 -0400

    cementing the basic ... objects and printing

--- js/main.js
+++ js/main.js
@@ -3,15 +3,70 @@
  * Overall Value
  * Total Assests = Liabilities + Equity
  */
+ // This is a variable
+ // all variables are declared with var
+ var title = 'Building Wealth'; // semi-colon ends the declaration
+
+// store the element by id
+ var elTitle = document.getElementById('title');
+ elTitle.textContent = title; // set the value of the element
 
  // the options for which forluma is chosen
- // array
+ // array constructor
  var formula = new Array(
      'assests',
      'one-tenth',
      'debt-payment',
      'growth');
 
+var elFormula = document.getElementById('asset-label');
+elFormula.textContent = formula[0]; // get array by key (index)
+
+// this is also an Array
+// array literal
+var newFormula = [ // square brackets
+    'assets', // comma delimited
+    'how to build wealth',
+    'debt-payment',
+    'growth',
+    'create a money tree' // no comma on last item
+];
+// object literal
+// creating an individual object
+var progression = { // curly brackets
+    name: 'wealth level', // key value pairs seperated by a colon
+    goal: 1000000, // name the indecies (keys)
+    level: 400,
+    workon: 'creating money tree'
+}; // same rules as arrays
+
+// print progession object title
+var pName = document.getElementById('progress-title');
+pName.textContent = progression.name;
+
+// print goal label
+var pGoalLabel = document.getElementById('progress-goal-label');
+pGoalLabel.textContent = pGoalLabel.name;
+
+// print progression level label
+
+// print progression workon label
+
+// print progression goal value
+var pGoalValue = document.getElementById('progress-goal');
+pGoalValue.textContent = progression.goal;
+
+// print progression level value
+var pLevelValue = document.getElementById('progress-level');
+pLevelValue.textContent = progression.level;
+
+// print progression workon value
+var pWorkonValue = document.getElementsByClassName('progress-workon');
+pWorkonValue.textContent = progression.workon;
+
+
+
+// Object
 // calculates the amount of assets for a business
 // function
 // returns assests
